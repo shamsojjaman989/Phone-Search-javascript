3bceba77d8ec8c7b40067f40b5059d0086826028
Muhammad Shamsojjaman <mdshamsojjaman989@gmail.com>
2022-11-15 12:33:35 +0600

    search field added

--- js/phone.js
+++ js/phone.js
@@ -1,3 +1,14 @@
+// search Phone in input 
+const searchPhoneData = () => {
+    // console.log('checked')
+    const searchInput = document.getElementById('search-field').value;
+    console.log(searchInput)
+    loadPhoneData(searchInput);
+    document.getElementById('search-field').value = '';
+}
+
+
+// load data from api 
 const loadPhoneData = searchPhone => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchPhone}`
     fetch(url)
@@ -15,9 +26,12 @@ const displayPhone = phones => {
                 <img class="img-fluid w-50 mx-auto p-4" src="${phone.image}"alt="reload">
                 <div class="card-body">
                     <h5 class="card-title">Name: ${phone.phone_name}</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <h5 class="card-title">Brand: ${phone.brand}</h5>
+                    <p class="card-text">slug: ${phone.slug}.</p>
                 </div>
+                <button class="w-25 text-center mx-auto mb-3">Details</button>
             </div>
+            
         `;
         cardGrp.appendChild(cards);
         console.log(phone);
